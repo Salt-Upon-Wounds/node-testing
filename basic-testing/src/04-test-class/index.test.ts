2bf46537606e8ccb2a8e8876c06f5f8eb8d9956f
Kirill <kiw197204@gmail.com>
2025-03-18 00:10:39 +0400

    refactor: dryer code

--- basic-testing/src/04-test-class/index.test.ts
+++ basic-testing/src/04-test-class/index.test.ts
@@ -1,53 +1,52 @@
-import { getBankAccount } from '.';
+import { BankAccount, getBankAccount } from '.';
 import _ from 'lodash';
 
+let BankAcc1: BankAccount;
+let BankAcc2: BankAccount;
+
 describe('BankAccount', () => {
+  beforeEach(() => {
+    BankAcc1 = getBankAccount(123);
+    BankAcc2 = getBankAccount(321);
+  });
+
   afterEach(() => {
     jest.restoreAllMocks();
   });
 
   test('should create account with initial balance', () => {
-    const BankAcc = getBankAccount(123);
-    expect(BankAcc.getBalance()).toBe(123);
+    expect(BankAcc1.getBalance()).toBe(123);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    const BankAcc = getBankAccount(123);
-    expect(() => BankAcc.withdraw(1233)).toThrow(
-      `Insufficient funds: cannot withdraw more than ${BankAcc.getBalance()}`,
+    expect(() => BankAcc1.withdraw(1233)).toThrow(
+      `Insufficient funds: cannot withdraw more than ${BankAcc1.getBalance()}`,
     );
   });
 
   test('should throw error when transferring more than balance', () => {
-    const BankAcc1 = getBankAccount(123);
-    const BankAcc2 = getBankAccount(321);
     expect(() => BankAcc1.transfer(1233, BankAcc2)).toThrow(
       `Insufficient funds: cannot withdraw more than ${BankAcc1.getBalance()}`,
     );
   });
 
   test('should throw error when transferring to the same account', () => {
-    const BankAcc1 = getBankAccount(123);
     expect(() => BankAcc1.transfer(12, BankAcc1)).toThrow(`Transfer failed`);
   });
 
   test('should deposit money', () => {
-    const BankAcc = getBankAccount(123);
-    BankAcc.deposit(123);
-    expect(BankAcc.getBalance()).toBe(246);
+    BankAcc1.deposit(123);
+    expect(BankAcc1.getBalance()).toBe(246);
   });
 
   test('should withdraw money', () => {
-    const BankAcc = getBankAccount(123);
-    BankAcc.withdraw(123);
-    expect(BankAcc.getBalance()).toBe(0);
+    BankAcc1.withdraw(123);
+    expect(BankAcc1.getBalance()).toBe(0);
   });
 
   test('should transfer money', () => {
-    const BankAcc1 = getBankAccount(123);
-    const BankAcc2 = getBankAccount(123);
     BankAcc1.transfer(123, BankAcc2);
-    expect(BankAcc2.getBalance()).toBe(246);
+    expect(BankAcc2.getBalance()).toBe(444);
     expect(BankAcc1.getBalance()).toBe(0);
   });
 
@@ -64,16 +63,14 @@ describe('BankAccount', () => {
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
-    const BankAcc = getBankAccount(123);
-    jest.spyOn(BankAcc, 'fetchBalance').mockReturnValue(Promise.resolve(42));
-    await BankAcc.synchronizeBalance();
-    expect(BankAcc.getBalance()).toBe(42);
+    jest.spyOn(BankAcc1, 'fetchBalance').mockReturnValue(Promise.resolve(42));
+    await BankAcc1.synchronizeBalance();
+    expect(BankAcc1.getBalance()).toBe(42);
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
     jest.spyOn(_, 'random').mockReturnValue(0);
-    const BankAcc = getBankAccount(123);
-    expect(BankAcc.synchronizeBalance()).rejects.toThrow(
+    expect(BankAcc1.synchronizeBalance()).rejects.toThrow(
       'Synchronization failed',
     );
   });
--- basic-testing/src/06-mocking-node-api/index.test.ts
+++ basic-testing/src/06-mocking-node-api/index.test.ts
@@ -3,6 +3,9 @@ import fsPromises from 'fs/promises';
 import fs from 'fs';
 import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
 
+jest.spyOn(global, 'setTimeout');
+jest.spyOn(global, 'setInterval');
+
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
     jest.useFakeTimers();
@@ -13,14 +16,12 @@ describe('doStuffByTimeout', () => {
   });
 
   test('should set timeout with provided callback and timeout', () => {
-    jest.spyOn(global, 'setTimeout');
     const callback = jest.fn();
     doStuffByTimeout(callback, 10000);
     expect(setTimeout).toHaveBeenLastCalledWith(callback, 10000);
   });
 
   test('should call callback only after timeout', () => {
-    jest.spyOn(global, 'setTimeout');
     const callback = jest.fn();
     doStuffByTimeout(callback, 10000);
     expect(callback).not.toBeCalled();
@@ -39,20 +40,16 @@ describe('doStuffByInterval', () => {
   });
 
   test('should set interval with provided callback and timeout', () => {
-    jest.spyOn(global, 'setInterval');
     const callback = jest.fn();
     doStuffByInterval(callback, 10000);
     expect(setInterval).toHaveBeenLastCalledWith(callback, 10000);
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    jest.spyOn(global, 'setInterval');
     const callback = jest.fn();
     doStuffByInterval(callback, 10000);
     expect(callback).not.toBeCalled();
-    jest.runOnlyPendingTimers();
-    jest.runOnlyPendingTimers();
-    jest.runOnlyPendingTimers();
+    jest.advanceTimersToNextTimer(3);
     expect(callback).toBeCalledTimes(3);
   });
 });
