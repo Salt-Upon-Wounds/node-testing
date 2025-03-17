5280e632d06f0cafeb66dfefecf9d25dce2450ba
Kirill <kiw197204@gmail.com>
2025-03-17 12:17:45 +0400

    feat: snapshot testing tests

--- basic-testing/src/08-snapshot-testing/index.test.ts
+++ basic-testing/src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,38 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+const values1 = [1, 2, 3, 4, 5, 6, 7];
+const values2 = [9, 2, 9, 4, 5, 9, 7];
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(values1)).toStrictEqual({
+      next: {
+        next: {
+          next: {
+            next: {
+              next: {
+                next: {
+                  next: {
+                    next: null,
+                    value: null,
+                  },
+                  value: 7,
+                },
+                value: 6,
+              },
+              value: 5,
+            },
+            value: 4,
+          },
+          value: 3,
+        },
+        value: 2,
+      },
+      value: 1,
+    });
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(values2)).toMatchSnapshot();
   });
 });
